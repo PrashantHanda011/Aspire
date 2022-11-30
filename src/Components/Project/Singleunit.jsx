a39826b7970c06e6c93fd81342c526d8a328b87d
satwik011 <satwiknakhate2000@gmail.com>
2022-11-30 14:39:50 +0530

    modal fixed

--- src/Components/Project/Singleunit.jsx
+++ src/Components/Project/Singleunit.jsx
@@ -1,27 +1,26 @@
-import React,{useState} from 'react'
-import {Row,Container,Col} from 'react-bootstrap'
-import '../../Assets/Project/property.css'
-import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { PostPriceData } from '../API/Api';
+import React, { useState } from "react";
+import { Row, Container, Col } from "react-bootstrap";
+import "../../Assets/Project/property.css";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { PostPriceData } from "../API/Api";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 function Singleunit(props) {
- 
   const [open, setOpen] = React.useState(false);
   const [open1, setOpen1] = React.useState(false);
   const handleOpen = () => setOpen(true);
@@ -29,47 +28,55 @@ function Singleunit(props) {
   const handleOpen1 = () => setOpen1(true);
   const handleClose1 = () => setOpen1(false);
   const [ApplicationData, setApplicationData] = useState({
-    name:"",
-    email:"",
-    phone:""
-  })
+    name: "",
+    email: "",
+    phone: "",
+  });
 
-  const [disable, setdisable] = useState(false)
+  const [disable, setdisable] = useState(false);
 
-  const handleChange=(e)=>{
-    let {name,value}=e.target
-    setApplicationData({...ApplicationData,[name]:value})
-  }
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    setApplicationData({ ...ApplicationData, [name]: value });
+  };
 
- const PostData = async()=>{
-  try {
-    await PostPriceData(ApplicationData);
-    setdisable(true)
-    handleClose()
-  } catch (error) {
-    console.log(error)
-  }
- }
+  const PostData = async () => {
+    try {
+      await PostPriceData(ApplicationData);
+      setdisable(true);
+      handleClose();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
-    <div className='Property-singleunit'>
-        <div className='Property-singleunit-img'>
+      <div className="Property-singleunit">
+        <div className="Property-singleunit-img">
           <figure>
-            <img src={props?.data?.floorPlan} onClick={handleOpen1} className='w-100' alt="" />
+            <img
+              src={props?.data?.floorPlan}
+              onClick={handleOpen1}
+              className="w-100"
+              alt=""
+            />
           </figure>
         </div>
-        <div className='Property-singleunit-desc'> {props?.data?.range}</div>
-        <div className='Property-singleunit-desc'>{props?.data?.size}</div>
-        <div className='Property-singleunit-desc'>
-            <button disabled={disable}  className="getprice-btn" onClick={handleOpen}>
-              get Price
-            </button>
+        <div className="Property-singleunit-desc"> {props?.data?.range}</div>
+        <div className="Property-singleunit-desc">{props?.data?.size}</div>
+        <div className="Property-singleunit-desc">
+          <button
+            disabled={disable}
+            className="getprice-btn"
+            onClick={handleOpen}
+          >
+            get Price
+          </button>
         </div>
-    </div>
+      </div>
 
-
-    <Modal
+      <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         open={open}
@@ -81,32 +88,51 @@ function Singleunit(props) {
         }}
       >
         <Fade in={open}>
-        <div className="career-modal-single">
-        <h4>Price Inquiry Form</h4>
-        <hr />
-          <form className='d-flex flex-wrap col-12 justify-content-between'>
-                      <div class="form-group col-5 mb-4">
-                        <label for="exampleInputEmail1"> Name </label>
-                        <input type="text"   class="form-control" name="name" onChange={handleChange} id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                      </div>                    
-                      <div class="form-group col-5">
-                        <label for="exampleInputEmail1">Phone Number</label>
-                        <input type="tel"  class="form-control" id="exampleInputEmail1" onChange={handleChange} name="phone" aria-describedby="emailHelp"    />
-                      </div> 
-                      
-                      <div class="form-group col-5">
-                        <label for="exampleInputEmail1">Email</label>
-                        <input type="email"  class="form-control" onChange={handleChange} id="exampleInputEmail1" aria-describedby="emailHelp"  name="email"  />
-                      </div>  
+          <div className="career-modal-single">
+            <h4>Price Inquiry Form</h4>
+            <hr />
+            <form className="d-flex flex-wrap col-12 justify-content-between">
+              <div class="form-group col-5 mb-4">
+                <label for="exampleInputEmail1"> Name </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="name"
+                  onChange={handleChange}
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                />
+              </div>
+              <div class="form-group col-5">
+                <label for="exampleInputEmail1">Phone Number</label>
+                <input
+                  type="tel"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  onChange={handleChange}
+                  name="phone"
+                  aria-describedby="emailHelp"
+                />
+              </div>
 
-                  </form>
-                  <button onClick={PostData} className='career-single-btn mt-3' >
-                    Submit
-                  </button>
-        </div>
+              <div class="form-group col-5">
+                <label for="exampleInputEmail1">Email</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  onChange={handleChange}
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  name="email"
+                />
+              </div>
+            </form>
+            <button onClick={PostData} className="career-single-btn mt-3">
+              Submit
+            </button>
+          </div>
         </Fade>
       </Modal>
-
 
       <Modal
         aria-labelledby="transition-modal-title"
@@ -120,14 +146,13 @@ function Singleunit(props) {
         }}
       >
         <Fade in={open1}>
-        <div className="career-modal-single" >
-          <img src={props?.data?.floorPlan} className="w-100" alt="no img" />
-        </div>
+          <div className="career-modal-single">
+            <img src={props?.data?.floorPlan} className="w-100" alt="no img" />
+          </div>
         </Fade>
       </Modal>
-    
     </>
-  )
+  );
 }
 
-export default Singleunit
+export default Singleunit;
