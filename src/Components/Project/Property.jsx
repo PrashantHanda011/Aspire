ad4b00e8ed86bea80bbb2112603db6778ae35651
satwik011 <satwiknakhate2000@gmail.com>
2022-12-06 14:26:29 +0530

    changes project page

--- src/Components/Project/Property.jsx
+++ src/Components/Project/Property.jsx
@@ -491,7 +491,7 @@ function Property() {
               </Col>
             </Row>
 
-            <Row id="amenities">
+            {/* <Row id="amenities">
               <Col className="property-amenity" lg={8}>
                 <h5>Project USP</h5>
                 <hr />
@@ -511,7 +511,7 @@ function Property() {
                   </Col>
                 </Row>
               </Col>
-            </Row>
+            </Row> */}
             {/* location */}
             <Row id="location">
               <Col className="property-location" lg={8}>
